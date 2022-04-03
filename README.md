b28ff465de5bd9536ba36e282fd53851fdff0a84
k sanketh kumar <k.sanketh123@gmail.com>
2022-04-04 01:57:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Example Application - Complimentr
+
+## Use this on Glitch
+
+[Remix on Glitch](https://sankeths-complimentr.glitch.me/)
+
+
+## Local Installation
+
+create .env with your [Twilio](https://twilio.com) credentials.
+
+## Running the application
+
+* `npm install`
+* `npm start`
+
+### In Development mode
+
+* Run [ngrok](https://ngrok.com/) on port 3000
+* Visit your ngrok url!
---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const TwilioClient = require("twilio");
+const app = express();
+const port = 3000;
+
+const client = TwilioClient();
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+// This is a single page application and it's all rendered in public/index.html
+app.use(express.static("public"));
+// Parse the body of requests automatically
+app.use(bodyParser.json());
+
+app.get("/api/compliments", async (req, res) => {
+  const sentMessages = await client.messages.list({ from: twilioPhoneNumber });
+  const compliments = sentMessages.map((message) => message.body);
+  res.json(compliments);
+});
+
+app.post("/api/compliments", async (req, res) => {
+  const to = req.body.to;
+  const from = process.env.TWILIO_PHONE_NUMBER;
+  const body = `${req.body.sender} says: ${req.body.receiver} is ${req.body.compliment}. See more compliments at ${req.headers.referer}`;
+  try {
+    await client.messages.create({ to, from, body });
+  } catch (err) {
+    res.status(err.status).json({ success: false, message: err.message });
+  }
+  res.json({ success: true });
+});
+
+app.listen(port, () => console.log(`Prototype is listening on port ${port}!`));
